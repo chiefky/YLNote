8a6d7d805f0b2d1f0d0f53f216f7d3275cf9bce1
yuki <374949723@qq.com>
2021-01-12 19:20:08 +0800

    1.13 todo

--- YLNote/Note/UIKit/YLUIKitNoteManger.m
+++ YLNote/Note/UIKit/YLUIKitNoteManger.m
@@ -19,21 +19,6 @@
     return instance;
 }
 
-//+ (NSDictionary *)allNotes {
-//    return         @{
-//        @"group":@"UIKit",
-//        @"questions":@[
-//                @"UIView 和 CALayer 是什么关系",
-//                @"Bounds 和 Frame 的区别",
-//                @"setNeedsDisplay 和 layoutIfNeeded 两者是什么关系",
-//                @"谈谈对UIResponder的理解",
-//                @"loadView的作用",
-//                @"使用 drawRect有什么影响",
-//                @"keyWindow 和 delegate的window有何区别"
-//        ]
-//    };
-//}
-
 + (NSDictionary *)allNotes {
     return @{
         @"group":@"UIKit",
--- YLNote/Note/Web/YLWebNoteManager.m
+++ YLNote/Note/Web/YLWebNoteManager.m
@@ -19,21 +19,6 @@
     return instance;
 }
 
-//+ (NSDictionary *)allNotes {
-//    return         @{
-//        @"group":@"Web",
-//        @"questions":@[
-//                @"UIView 和 CALayer 是什么关系",
-//                @"Bounds 和 Frame 的区别",
-//                @"setNeedsDisplay 和 layoutIfNeeded 两者是什么关系",
-//                @"谈谈对UIResponder的理解",
-//                @"loadView的作用",
-//                @"使用 drawRect有什么影响",
-//                @"keyWindow 和 delegate的window有何区别"
-//        ]
-//    };
-//}
-
 + (NSDictionary *)allNotes {
     return @{
         @"group":@"Web",
@@ -46,11 +31,11 @@
                     @"type": @(0)
                 },
                 @{
-                                  @"description":@"使用 WKWedView 时遇到过哪些问题",
-                                  @"answer":@"testWKWebViewCookie",
-                                  @"class": NSStringFromClass(self),
-                                  @"type": @(0)
-                              }
+                    @"description":@"使用 WKWedView 时遇到过哪些问题",
+                    @"answer":@"testWKWebViewCookie",
+                    @"class": NSStringFromClass(self),
+                    @"type": @(0)
+                }
             ]
     };
 }
